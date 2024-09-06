30f2dd401c5163d3ea778147e4c038e9c83d81c6
Diego Molina <dimolinas@unal.edu.co>
2024-09-05 19:08:50 -0500

    Initial Commit

---
+++ LearnActors/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LearnActors
+//
+//  Created by Diegoe012 on 5/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LearnActors/LearnActorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnActorsApp.swift
+//  LearnActors
+//
+//  Created by Diegoe012 on 5/09/24.
+//
+
+import SwiftUI
+
+@main
+struct LearnActorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
